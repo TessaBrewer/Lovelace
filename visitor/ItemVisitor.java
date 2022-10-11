0a9b7bf5b02143dbf423a8ce2d60b10e7dc4dd49
highqualitybean <tjobs959@gmail.com>
2022-10-11 19:49:30 +0100

    Update ItemVisitor.java

--- visitor/ItemVisitor.java
+++ visitor/ItemVisitor.java
@@ -1,8 +1,28 @@
 package visitor;
 
-import products.Item;
+import products.impl.Hammer;
+import products.impl.Nails;
+import products.impl.Saw;
+import products.materials.Brass;
+import products.materials.Steel;
+import products.materials.Titanium;
 
-public class ItemVisitor {
-    public void visit(Item i) {
+public abstract class ItemVisitor {
+    public void visit(Hammer hammer) {
+    }
+    
+    public void visit(Nails nails) {
+    }
+    
+    public void visit(Saw saw) {
+    }
+    
+    public void visit(Brass brass) {
+    }
+
+    public void visit(Steel steel) {
+    }
+    
+    public void visit(Titanium titanium) {
     }
 }
